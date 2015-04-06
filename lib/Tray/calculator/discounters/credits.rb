dde8cc205d1a73b73236e3f46475f3d23579ea72
Christopher Ostrowski <chris@madebyfunction.com>
2015-04-06 18:28:27 -0500

    Fixed cart totalling when credits are more than cart total.

--- lib/Tray/calculator/discounters/credits.rb
+++ lib/Tray/calculator/discounters/credits.rb
@@ -5,7 +5,7 @@ module Tray
         def call
           return unless @cart.customer
           @cart.customer.credits.sort_by(&:created_at).each do |credit|
-            next unless credit.is_available?
+            next unless credit.is_available? && credit.amount_remaining_in_cents > 0
             credit_registers = applicable_registers(credit)
             next unless credit_registers.count > 0
 
@@ -23,8 +23,11 @@ module Tray
           credit_amount = credit.amount_remaining_in_cents
           
           registers.each do |reg|
-            discount    = ([credit_amount, 0].max - reg.line_items_total).abs
-            discount    = reg.line_items_total - discount
+            if reg.line_items_total - credit_amount >= 0
+              discount = credit_amount
+            else
+              discount = reg.line_items_total
+            end
             credit_amount = credit_amount - discount
             reg.applied_credits.push({credit: credit, amount: discount, type: :credit})
           end
