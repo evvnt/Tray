2088a4d8fda86c44444365a66b6b0141558ef914
Derek Graham <derek@mynorth.com>
2016-06-15 15:10:54 -0400

    Do not include fee in package adder. Package fees are added to the total separately

--- lib/Tray/calculator/adders/package.rb
+++ lib/Tray/calculator/adders/package.rb
@@ -7,18 +7,11 @@ module Tray
           def call(cart)
             totals = []
             cart.line_items.by_ticket_package.each do |package|
-              totals.push Tray::Calculator::Register.new(package: package, line_items_total: package.entity.customer_price_in_cents, line_items: package)
+              totals.push Tray::Calculator::Register.new(package: package, line_items_total: package.entity.price_in_cents, line_items: package)
             end
 
             return totals
           end
-
-          # def reduce_line_items(items)
-          #   items.reduce(0) do |memo, item|
-          #     package_price = item.entity.customer_price_in_cents
-          #     #memo += package_price * (item.quantity || 1) # No quantities with packages yet
-          #   end
-          # end
 
         end
       end
