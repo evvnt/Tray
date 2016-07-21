d1867b12621ec2290a2e17a69303d865d56a51dc
Derek Graham <derek@mynorth.com>
2016-07-21 14:50:28 -0400

    Gift card reduction by package definition working now

--- lib/Tray/calculator/discounters/reduction_code.rb
+++ lib/Tray/calculator/discounters/reduction_code.rb
@@ -2,6 +2,12 @@ module Tray
   module Calculator
     module Discounters
       class ReductionCode < Discounter
+
+        def initialize(cart, registers)
+          @ticket_count = 0
+          super(cart, registers)
+        end
+
         def call
           @cart.reduction_codes.each do |code|
             #next unless code.gift_card.current_value_in_cents > 0
@@ -13,22 +19,26 @@ module Tray
 
         def applicable_registers(code)
           @registers.select do |register|
-            next true if code.gift_card.organization_id == register.event.organization_id
+            next false unless code.gift_card.organization_id == register.event.organization_id
             if code.gift_card.card_type == 'Package'
               next false if code.gift_card.remaining_tickets == 0
               # Check events in package against the ones in the register
-              next true if gift_card_line_item_amount(register.event.id, register.line_item, code.gift_card)
+              next true if gift_card_line_item_amount(register.event.id, register.line_items, code.gift_card)[:total] > 0
             end
+            next true
           end
         end
 
         def apply_reduction_code_registers(code, registers)
-          # if code.gift_card.card_type = 'Package'
-          #   # TODO: make deductions from total based on ticket. Keep track of # of tickets and the associated discount amount. Decrements row will look like: [{amount: XXXX, ticket_count: XXXX, customer_order_id: XXXXX},...]
-          #   - for each ticket, check type/price level
-          #   - add to applied_reduction_codes with amount of ticket+fee and new type
-          #     reg.applied_reduction_codes.push({card: code, amount: discount, type: :ticket, ticket_count: XX})  # -- each one of these should count as a deduction of one ticket in the card's record
-          # else
+          if code.gift_card.card_type == 'Package'
+            @ticket_count = 0
+            registers.each do |reg|
+              discount = gift_card_line_item_amount(reg.event.id, reg.line_items, code.gift_card)
+              if discount[:total] > 0
+                reg.applied_reduction_codes.push({card: code, amount: discount[:total], type: :ticket, ticket_count: discount[:ticket_count]})
+              end
+            end
+          else
             card_amount = code.gift_card.current_value_in_cents
             registers.each do |reg|
               if reg.line_items_total - card_amount >= 0
@@ -39,12 +49,27 @@ module Tray
               card_amount = card_amount - discount
               reg.applied_reduction_codes.push({card: code, amount: discount, type: :credit})
             end
-          # end
+          end
         end
 
         private
-        def gift_card_line_item_amount(event_id, line_item, gift_card)
-          # TODO: finish this, look at cart model method
+        def gift_card_line_item_amount(event_id, line_items, gift_card)
+          # Figure out which ticket(s) are discounted and add up their cost and fees
+          available_tickets = gift_card.remaining_tickets
+          register_ticket_count = 0
+          line_items_total = 0
+          line_items.each do |item|
+            gift_card.valid_events.select { |e| e[:event_id] == event_id }.each do |v|
+              v[:ticket_types].each { |tt|
+                if (tt['ticket_type_id'] == "" || item.entity.id == tt['ticket_type_id'].to_i) && (tt['price_level'] == "" || item.options['price_level'] == tt['price_level']) && @ticket_count < available_tickets
+                  line_items_total += item.entity.price_for_level_in_cents_without_fee(item.options['price_level']) + item.entity.fee_for_level_in_cents(item.options['price_level'])
+                  register_ticket_count += 1
+                  @ticket_count += 1
+                end
+              }
+            end
+          end
+          return {total: line_items_total, ticket_count: register_ticket_count}
         end
       end
     end
