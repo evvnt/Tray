f4b9865a59745d56716b9de6645ec4880c4fb8e8
Nick Miller <nick@nmiller.info>
2019-07-31 14:15:22 -0400

    Ensure MP fees are included only for TicketTypes
    [#167375164]

--- lib/Tray/calculator/fees/ticket_fee.rb
+++ lib/Tray/calculator/fees/ticket_fee.rb
@@ -11,7 +11,7 @@ module Tray
                 entity = item.entity
                 entity = entity.ticket_type if entity.is_a?(EventTicketType)
 
-                if entity.respond_to?(:processing_fee)
+                if entity.is_a?(TicketType)
                   total_fees += entity.processing_fee(entity.price_for_level_in_cents_without_fee(item.options[:price_level]) - item.discount_total)
                 elsif entity.is_a?(TicketPackage)
                   total_fees += entity.package_fee_in_cents
